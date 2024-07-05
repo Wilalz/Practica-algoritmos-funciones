b96a185b9cef101363296d5d4b22fbe7a66bedab
WilAlz <wilalzatep@gmail.com>
2024-07-05 00:17:35 -0500

    [FEAT] Agregar 4 nuevos botonces y sus funciones

--- app.js
+++ app.js
@@ -25,3 +25,48 @@ function suma () {
     let segundoNumero = parseInt(prompt ("Ingresa el segundo número:"));
     alert(primerNumero + segundoNumero);
 }
+
+
+//  8. Crear una función que reciba un número como parámetro y devuelva el doble de ese número.
+function doble(){
+    let num = parseInt(prompt("Ingresa un numero para duplicarlo"));
+    let doble = num * num
+    alert(`El doble de ${num} es: ${doble}`);
+}
+
+//  9. Crear una función que reciba tres números como parámetros y devuelva su promedio.
+function promedioDeTres() {
+    let primero = parseInt(prompt("Ingresa el primer número"));
+    let segundo = parseInt(prompt("Ingresa el segundo número"));
+    let tercero = parseInt(prompt("Ingresa el tercer número"));
+    let prom = ((primero + segundo + tercero)/3).toFixed(2);
+    alert(`El promedio entre ${primero}, ${segundo} y ${tercero} es: ${prom}`); 
+}
+
+//  10. Crear una función que reciba dos números como parámetros y devuelva el mayor de ellos.
+function mayor() {
+    let primero = parseInt(prompt('Ingrese el primer número'));
+    let segundo = parseInt(prompt('Ingrese elsegundo número'));
+    if (primero === segundo){
+        alert(`Los dos números son iguales`);
+    } else if (primero > segundo){
+        alert(`El número mayor es: \n${primero}`);
+    } else {
+        alert(`El número mayor es: \n${segundo}`);
+    }
+}
+
+//  11. Crear una función que reciba un número como parámetro y devuelva el resultado de multiplicar ese número por sí mismo
+// MODIFICADA - Cree una funcion que recibe 2 parametros y devuelve el resultado de su multimpliación
+
+function multiplicarNumeros (multiploUno, multiploDos) {
+    return multiploUno * multiploDos;
+}
+
+function multiplicar (){
+    let primero = parseInt(prompt('Ingrese el primer número'));
+    let segundo = parseInt(prompt('Ingrese el segundo número'));
+    let resultado = multiplicarNumeros(primero, segundo);
+    alert(`El resultado de multiplicar ${primero} por ${segundo} es:\n ${resultado}`);
+}
+
